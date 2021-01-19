2bbdbd3cd37785e55405f016cbf81abd82809c03
GyeongYeonAn <ruddus7444@naver.com>
2021-01-19 15:59:38 +0900

    MockMvc 가게 목록 Get Test

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTest.java
@@ -11,14 +11,15 @@ class RestaurantTest {
 
     @Test
     public void creation(){
-        Restaurant restaurant = new Restaurant("Bob zip", "");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "");
+        assertThat(restaurant.getId(), is(1004L));
         assertThat(restaurant.getName(), is("Bob zip"));
         assertThat(restaurant.getAddress(), is("Seoul"));
     }
 
     @Test
     public void information() {
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
         assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
     }
 }
