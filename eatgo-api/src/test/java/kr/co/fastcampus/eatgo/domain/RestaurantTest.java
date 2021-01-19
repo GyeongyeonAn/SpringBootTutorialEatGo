dc76e51550d71f78b04faae5f91cf18447dfdd99
GyeongYeonAn <ruddus7444@naver.com>
2021-01-19 15:07:53 +0900

    TDD 흐름 이해하기

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTest.java
@@ -0,0 +1,24 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@SpringBootTest
+class RestaurantTest {
+
+    @Test
+    public void creation(){
+        Restaurant restaurant = new Restaurant("Bob zip", "");
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+}
