e1d17e425d24bf4adbecb4c739f730548c244cf0
GyeongYeonAn <ruddus7444@naver.com>
2021-01-29 16:31:46 +0900

    메뉴 관리 * 메뉴를 업데이트, 삭제 기능

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 @Getter
@@ -22,4 +24,7 @@ public class MenuItem {
 
     private String name;
 
+    @Transient // DB에 넣지 않음
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private boolean destroy;
 }
--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
     List<MenuItem> findAllByRestaurantId(Long restaurantId);
+
+    void deleteById(Long id);
 }
