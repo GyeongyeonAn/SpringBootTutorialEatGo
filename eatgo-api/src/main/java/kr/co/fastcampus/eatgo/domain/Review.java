6035c595a7cb9532394b91158ef3c831a1ccb319
GyeongYeonAn <ruddus7444@naver.com>
2021-01-29 19:38:26 +0900

    리뷰 작성 * 리뷰 Entity, Repository, controller, Service 생성 * 리뷰 create 구현

---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
@@ -0,0 +1,33 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    private Integer score;
+
+    @NotEmpty
+    private String description;
+}
