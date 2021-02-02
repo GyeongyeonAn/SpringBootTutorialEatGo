1bf17a2980376c5c7e5e1715001fe1bb02dea511
GyeongYeonAn <ruddus7444@naver.com>
2021-02-02 16:48:25 +0900

    프로젝트 분리 1 * customer-api 모듈 생성 * admin-api application, interface 복사 붙어넣기

---
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/EatgoCustomerApiApplication.java
@@ -0,0 +1,12 @@
+package kr.co.fastcampus.eatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatgoCustomerApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EatgoCustomerApiApplication.class, args);
+	}
+
+}
---
+++ eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/MenuItemControllerTest.java
@@ -0,0 +1,36 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.MenuItemService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MenuItemController.class)
+class MenuItemControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MenuItemService menuItemService;
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        mvc.perform(patch("/restaurants/1/menuItems")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[]"))
+                .andExpect(status().isOk());
+
+        verify(menuItemService).bulkUpdate(eq(1L), any());
+    }
+
+}
