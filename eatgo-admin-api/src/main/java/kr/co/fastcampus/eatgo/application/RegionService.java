2125abeea2005e303e45ef2260269b6e6d6bf202
GyeongYeonAn <ruddus7444@naver.com>
2021-02-05 15:20:41 +0900

    지역정보 관리 기능 추가 * Region entity, controller, service 추가

---
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/RegionService.java
@@ -0,0 +1,30 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.Region;
+import kr.co.fastcampus.eatgo.domain.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RegionService {
+
+    private RegionRepository regionRepository;
+
+    @Autowired
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    public List<Region> getRegions() {
+        return regionRepository.findAll();
+    }
+
+    public Region addRegion(String name) {
+        Region region = Region.builder().name(name).build();
+        regionRepository.save(region);
+
+        return region;
+    }
+}
